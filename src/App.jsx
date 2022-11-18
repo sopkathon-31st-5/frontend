07cd1419c78ebc60d809c860c074a0f3aa2d5b6f
hayoun <fishingest@kookmin.ac.kr>
2022-11-18 18:32:21 +0900

    feat:Router, GlobalStyle

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import Router from './core/router';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './styles/global';
+import { theme, mixins } from './styles/theme';
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={{}}>
+        <Router />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
