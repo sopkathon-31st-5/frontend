f93bce997f241b6ddd4f41beaa5b2f5a29da5239
nahee-park <nahee.developer@gmail.com>
2022-11-20 05:59:02 +0900

    feat: Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,17 +4,43 @@ import Button from '../components/common/Button';
 import Header from '../components/common/Header';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
+import LoginForm from '../components/Form/LoginForm';
+import { useRecoilState } from 'recoil';
+import userData from '../states/atom/userData';
 
+const info = `명함 수정을 원하신다면,
+처음 기입했던 성함과 전화번호를 입력해주세요.`;
 function Login() {
   const navigate = useNavigate();
+  const [user, setUser] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [inputData, setInputData] = useRecoilState(userData);
   const handleClick = () => {
     navigate('/make_business_card');
+    setInputData({
+      ...inputData,
+      card: {
+        ...inputData.card,
+        name: user,
+        telNumber: phoneNumber,
+      },
+    });
   };
+
   return (
     <StMain>
       <Header step="LOGIN" />
       <Banner step="LOGIN" />
-      <Button onClick={handleClick}>명함 만들러 가기</Button>
+      <LoginForm
+        user={user}
+        phoneNumber={phoneNumber}
+        setUser={setUser}
+        setPhoneNumber={setPhoneNumber}
+      />
+      <StInfo>{info}</StInfo>
+      <Button onClick={handleClick} disabled={user.length === 0 || phoneNumber.length === 0}>
+        다음으로
+      </Button>
     </StMain>
   );
 }
@@ -25,4 +51,18 @@ const StMain = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
+`;
+const StInfo = styled.p`
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 120%;
+
+  text-align: center;
+
+  color: #707070;
+  white-space: pre-line;
+  margin-bottom: 32px;
+  margin-top: 120%;
 `;
