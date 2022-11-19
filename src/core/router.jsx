a3e0d9b7b9eeb80fc6f3e7f3a9e4c913ecaf3721
nahee-park <nahee.developer@gmail.com>
2022-11-19 22:53:00 +0900

    feat: add example component

--- src/core/router.jsx
+++ src/core/router.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from '../pages/Main';
 
 function Router() {
   return (
     <BrowserRouter>
-      <Routes>{/* <Route path="/" element={<Component />} /> */}</Routes>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
     </BrowserRouter>
   );
 }
---
+++ src/pages/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Main() {
+  return <StMain>Main</StMain>;
+}
+
+export default Main;
+
+const StMain = styled.main`
+  background: black;
+`;
