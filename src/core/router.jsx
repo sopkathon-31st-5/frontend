1150f7046e4a9d111279a09e8dbcae3bf6613cdf
nahee-park <nahee.developer@gmail.com>
2022-11-20 01:38:54 +0900

    feat: add router

--- src/core/router.jsx
+++ src/core/router.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from '../pages/Main';
+import Login from '../pages/Login';
+import BusinessCard from '../pages/BusinessCard';
+import Result from '../pages/Result';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/make_business_card" element={<BusinessCard />} />
+        <Route path="/result" element={<Result />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/BusinessCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function BusinessCard() {
+  return <div>BusinessCard</div>;
+}
+
+export default BusinessCard;
